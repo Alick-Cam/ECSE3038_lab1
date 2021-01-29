986f19d6c1698158441c6fc283bdee706a90f976
Alick Campbell <alickcampbell54321@gmail.com>
2021-01-29 16:55:14 -0500

    Added validatePassword(); to VersionControl

--- VersionControl/src/VersionControl.java
+++ VersionControl/src/VersionControl.java
@@ -5,10 +5,43 @@ public class VersionControl {
 		// TODO Auto-generated method stub
 		VersionControl obj = new VersionControl();
 		obj.hello();
+		System.out.println(obj.validatePassword("GreetingsFe11owKids"));
+
 
 	}
 
 	private void hello() {
-		System.out.print("ECSE3038 - Engineering IoT Systems");
+		System.out.println("ECSE3038 - Engineering IoT Systems");
+	}
+	
+	private boolean validatePassword(String password) {
+		boolean valid = false;
+		int checks = 0; //increment when an alphanumeric character is met
+		int numChecks = 0; // increment when a number character is met
+		
+		// check if length is greater than or equal to 8
+		if (password.length()>=8) {
+			// check for alphanumeric characters and retrieve numeric count
+			for ( int x = 0 ; x < password.length() ; x++) {
+				if(password.charAt(x) < 127 && password.charAt(x) > 32)  // Alphanumeric range of the ASCII chart 31 - 126
+					checks++;
+				if(password.charAt(x) < 58 && password.charAt(x) > 47) // Numeric range of ASCII chart is 48 - 57
+					numChecks++;
+			}
+			if(checks == password.length()) {
+				// check for at least 2 numbers
+				if(numChecks >= 2) 
+					valid = true;
+				 else 
+					valid = false;	
+			} else {
+				valid = false;
+			}
+				
+		} else {
+			valid = false;
+		}
+		
+		return valid;
 	}
 }

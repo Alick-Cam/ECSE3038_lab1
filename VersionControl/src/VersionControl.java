e66c51f01f774fec1ab42fa855a5ae5b7c7f9182
Alick Campbell <alickcampbell54321@gmail.com>
2021-01-29 16:05:32 -0500

    Added hello(); to VersionControl

---
+++ VersionControl/src/VersionControl.java
@@ -0,0 +1,14 @@
+
+public class VersionControl {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		VersionControl obj = new VersionControl();
+		obj.hello();
+
+	}
+
+	private void hello() {
+		System.out.print("ECSE3038 - Engineering IoT Systems");
+	}
+}
